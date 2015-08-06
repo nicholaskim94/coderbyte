b2f74effed222b855bc98e0ed04db6cd6a358071
Nicholas Kim <nicholaskim94@gmail.com>
2015-08-07 00:32:28 +0900

    Add LongestWord

---
+++ LongestWord/LongestWord.rb
@@ -0,0 +1,17 @@
+def LongestWord(sen)
+
+  # code goes here
+  @words = sen.split(' ')
+  @longestWord = @words
+  @words.each do |word|
+    if word.to_s.length > @longestWord.length
+      @longestWord = word
+	end
+  end
+  return @longestWord
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+LongestWord(STDIN.gets) 
